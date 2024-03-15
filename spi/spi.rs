0200fb6eefdc68ffe225ec92474bf03fcecf36ad
Jens Peter Secher <jpsecher@gmail.com>
2024-03-15 13:23:05 +0100

    Giving up for now

--- spi/spi.rs
+++ spi/spi.rs
@@ -1,34 +1,35 @@
 #![no_std]
 #![no_main]
-extern crate panic_halt;
-extern crate embedded_hal;
-extern crate rp2040_hal;
 
-// Ensure we halt the program on panic (if we don't mention this crate it won't
-// be linked)
+// Needs to be included.
 #[cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 use panic_halt as _;
 
 use rp2040_hal as hal;
 use hal::pac;  // Peripheral Access Crate
-use embedded_hal::digital::v2::OutputPin;
-use rp2040_hal::clocks::Clock;
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
 
-/// The linker will place this boot block at the start of our program image. We
-/// need this to help the ROM bootloader get our code up and running.
-/// Note: This boot block is not necessary when using a rp-hal based BSP
-/// as the BSPs already perform this step.
+// extern crate panic_halt;
+// extern crate embedded_hal;
+// extern crate rp2040_hal;
+// extern crate cortex_m;
+
+use hal::clocks::Clock;
+// use embedded_hal::digital::OutputPin;
+// use cortex_m::prelude::*;
+// use hal::fugit::RateExtU32;
+
+// Bootloader block
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
-/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables and the spinlock are initialised.
 #[rp2040_hal::entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
     let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
@@ -39,9 +40,8 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
+    // .ok()
     .unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let sio = hal::Sio::new(pac.SIO);  // Single-cycle I/O block
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
@@ -49,11 +49,29 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+    // // Dedicated pins are implicitly used by the spi driver if they are in the correct mode
+    // let spi_mosi = pins.gpio7.into_function::<hal::gpio::FunctionSpi>();
+    // let spi_miso = pins.gpio4.into_function::<hal::gpio::FunctionSpi>();
+    // let spi_sclk = pins.gpio6.into_function::<hal::gpio::FunctionSpi>();
+    // let spi = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_mosi, spi_miso, spi_sclk));
+    // // Exchange the uninitialised SPI driver for an initialised one
+    // let mut spi = spi.init(
+    //     &mut pac.RESETS,
+    //     clocks.peripheral_clock.freq(),
+    //     16.MHz(),
+    //     embedded_hal::spi::MODE_0,
+    // );
+
+    // Keep sending commands to the DAC, blink if ok
     let mut led_pin = pins.gpio25.into_push_pull_output();
+    // let core = pac::CorePeripherals::take().unwrap();
+    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     loop {
+        // if spi.write(&[0b0001_0111, 0b11111111]).is_ok() {
         led_pin.set_high().unwrap();
-        delay.delay_ms(250);
+        // }
+        timer.delay_ms(250);
         led_pin.set_low().unwrap();
-        delay.delay_ms(750);
+        timer.delay_ms(750);
     }
 }
